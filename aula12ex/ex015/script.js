f985d2a2cca133237a1d5716152260815333bf08
Gustavo <101855304+GustavoRCS@users.noreply.github.com>
2022-07-25 21:17:07 -0300

    verificador de idadejs

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,12 @@
-alert('Olá')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.querySelector('div#txtano')
+    var res = document.querySelector('div#res') 
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente')
+    } else {
+        window.alert('Tudo OK')
+    }
+
+}
