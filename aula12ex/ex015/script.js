2c6c6ceb94e932382c73e5dec93f82bce4a4cbcf
Gustavo <101855304+GustavoRCS@users.noreply.github.com>
2022-08-28 15:57:05 -0300

    atualizando meu java script

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -3,10 +3,10 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.querySelector('div#txtano')
     var res = document.querySelector('div#res') 
-    if (fano.value.length == 0 || fano.value > ano) {
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente')
     } else {
         window.alert('Tudo OK')
-    }
 
-}
+    }
+} 
