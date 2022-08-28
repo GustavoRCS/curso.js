05fe76f152157a6932612e643b98fcd08709ace7
Gustavo <101855304+GustavoRCS@users.noreply.github.com>
2022-08-28 17:26:30 -0300

    basico estudo calc anos de idade

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,12 +1,40 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.querySelector('div#txtano')
+    var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res') 
     if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente')
+        window.alert('[ERRO] Verifique os dados e tente novamente !')
     } else {
-        window.alert('Tudo OK')
-
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade < 10){
+                //criança 
+            } else if (idade < 21) {
+                //joven
+            } else if (idade < 50) {
+                //adulto
+            } else {
+                //idoso
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10){
+                //criança 
+            } else if (idade < 21) {
+                //joven
+            } else if (idade < 50) {
+                //adulto
+            } else {
+                //idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos um ${genero} com ${idade} anos.`
     }
 } 
