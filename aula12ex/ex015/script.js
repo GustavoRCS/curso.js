849a3166be1ed3e05254125a0fe0c2b1d8c179af
Gustavo <101855304+GustavoRCS@users.noreply.github.com>
2022-10-10 18:44:15 -0300

    atualização não lembro js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -15,26 +15,35 @@ function verificar() {
             genero = 'Homem'
             if (idade >=0 && idade < 10){
                 //criança 
+                img.setAttribute('src', 'bebe-menino.png')
             } else if (idade < 21) {
                 //joven
+                img.setAttribute('src', 'jovem-masculino.png')
             } else if (idade < 50) {
                 //adulto
+                img.setAttribute('src', 'homem-adulto.png')
             } else {
                 //idoso
+                img.setAttribute('src', 'idoso-masculino.png')
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade >=0 && idade < 10){
                 //criança 
+                img.setAttribute('src', 'bebe-menina.png')
             } else if (idade < 21) {
                 //joven
+                img.setAttribute('src', 'jovem-feminina.png')
             } else if (idade < 50) {
                 //adulto
+                img.setAttribute('src', 'mulher-adulta.png')
             } else {
                 //idoso
+                img.setAttribute('src', 'idosa-feminina.png')
             }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos um ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 } 
